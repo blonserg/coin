fba1d61e28d01a3825ab1f563616aa4e90bfb949
blonserg <sergforweb@gmail.com>
2021-11-25 13:17:53 +0300

    add edit form of profile

--- pages/profile.vue
+++ pages/profile.vue
@@ -6,7 +6,6 @@
       </v-col>
     </v-row>
     <v-row v-else>
-      {{ userProfile }}
       <v-col md="4">
         <v-card class="profile-info">
           <div class="profile-avatar">
@@ -20,9 +19,10 @@
             </v-avatar>
           </div>
           <div class="profile-name">
-            Владислав Борин
+            <span>{{ userProfile.profile.first_name }}</span>
+            <span>{{ userProfile.profile.last_name }}</span>
             <span class="profile-name_nick">
-              @usernameplace
+              ID: {{ userProfile.profile.id }}
             </span>
           </div>
           <v-divider />
@@ -31,11 +31,11 @@
               Тариф:
             </div>
             <div class="header-tariph premium">
-              PREMIUM
+              {{ userProfile.tariff.title }}
             </div>
           </div>
           <div class="profile-tariph_date">
-            до 24.09.2022
+            до {{ userProfile.tariff.end_date }}
           </div>
           <a href="" class="article-link">
             Продлить тариф
@@ -46,7 +46,7 @@
               {{ staticData.my_profile_ref_link }}
             </div>
             <div class="header-refs d-flex align-center">
-              <v-text-field ref="textToCopy" outlined readonly />
+              <v-text-field ref="textToCopy" :value="userProfile.profile.referral_link" outlined readonly />
               <button color="primary" class="header-refs_btn d-flex align-center justify-center" @click="copyText">
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M1.27842 8.9769L3.46623 6.78548C3.36079 7.41914 3.36079 8.0264 3.54531 8.66007L2.30643 9.90099C1.19934 11.0099 1.19934 12.5677 2.33279 13.7294C3.43987 14.8383 5.02142 14.8383 6.1285 13.7294L9.23888 10.5875C10.346 9.47855 10.346 7.89439 9.23888 6.75908C8.81714 6.33663 8.31631 6.07261 7.84185 5.9934C7.86821 5.91419 8 5.75578 8.10544 5.65017L8.94893 4.80528C9.39704 4.9901 9.8715 5.28053 10.2932 5.70297C11.9802 7.39274 11.9802 9.79538 10.2932 11.4851L7.05107 14.7327C5.36409 16.4224 2.99177 16.4224 1.27842 14.7327C-0.408563 13.0429 -0.408563 10.6667 1.27842 8.9769ZM5.75948 10.2706C4.07249 8.58086 4.07249 6.17822 5.75948 4.51485L8.97529 1.26733C10.6623 -0.422442 13.0346 -0.422442 14.7216 1.26733C16.4086 2.9571 16.4086 5.35974 14.7216 7.0231L12.5601 9.24092C12.6656 8.60726 12.6656 7.9736 12.4811 7.36634L13.7199 6.15181C14.827 5.0165 14.827 3.43234 13.6936 2.29703C12.5865 1.18812 11.0049 1.18812 9.89786 2.29703L6.78748 5.41254C5.6804 6.52145 5.6804 8.10561 6.78748 9.21452C7.20923 9.63696 7.71005 9.90099 8.18452 9.9802C8.15816 10.0858 8.02636 10.2178 7.92093 10.3234L7.07743 11.1683C6.62933 11.0099 6.18122 10.6931 5.75948 10.2706Z" fill="#6A6B79" />
@@ -71,8 +71,30 @@
         </v-card>
       </v-col>
       <v-col class="profile-form" md="8">
-        <div class="profile-ttl">
-          {{ staticData.my_profile_main_info }}
+        <div class="d-flex">
+          <div class="profile-ttl">
+            {{ staticData.my_profile_main_info }}
+          </div>
+          <div class="profile-edit">
+            <v-btn
+              v-if="editUser"
+              class="btn--edit"
+              plain
+              @click.native="editUserProfile"
+            >
+              Редактировать
+              <PencilSvg class="ml-2" />
+            </v-btn>
+            <v-btn
+              v-else
+              class="btn--edit"
+              plain
+              @click.native="changeUserProfile"
+            >
+              Сохранить
+              <PencilSvg class="ml-2" />
+            </v-btn>
+          </div>
         </div>
         <v-row>
           <v-col md="6" class="mb-10">
@@ -81,7 +103,7 @@
               v-model="userProfile.profile.first_name"
               :label="userProfile.profile.first_name || `Не заполнено`"
               solo
-              readonly
+              :disabled="editUser"
             />
           </v-col>
           <v-col md="6" class="mb-10">
@@ -90,7 +112,7 @@
               v-model="userProfile.profile.last_name"
               :label="userProfile.profile.last_name || `Не заполнено`"
               solo
-              readonly
+              :disabled="editUser"
             />
           </v-col>
         </v-row>
@@ -101,7 +123,7 @@
               v-model="userProfile.profile.telegram"
               :label="userProfile.profile.telegram || `@`"
               solo
-              readonly
+              :disabled="editUser"
             />
           </v-col>
           <v-col md="6" class="mb-10">
@@ -109,8 +131,9 @@
             <v-text-field
               v-model="userProfile.profile.instagram"
               :label="userProfile.profile.instagram || `@`"
+              :value="userProfile.profile.instagram || `@`"
               solo
-              readonly
+              :disabled="editUser"
             />
           </v-col>
         </v-row>
@@ -153,15 +176,6 @@
             </button>
           </div>
         </div>
-        <v-btn
-          class="btn d-none d-md-flex"
-          color="#2d7bf6"
-          x-large
-          height="48px"
-          @click.native="changeUserProfile"
-        >
-          Save
-        </v-btn>
       </v-col>
     </v-row>
   </div>
@@ -172,10 +186,12 @@ import StaticService from "~/services/StaticService";
 import UserService from "~/services/UserService";
 import HttpService from "~/services/HttpService";
 import LogoutSvg from "~~/components/svg/LogoutSvg";
+import PencilSvg from "~~/components/svg/PencilSvg";
 
 export default {
   components: {
-    LogoutSvg
+    LogoutSvg,
+    PencilSvg
   },
   data: () => ({
     cities: ["Sambir", "Lviv", "Kyiv"],
@@ -185,7 +201,8 @@ export default {
     tariffs: null,
     userChats: null,
     userSecuritySettings: null,
-    userProfileProjects: null
+    userProfileProjects: null,
+    editUser: true
   }),
   async fetch () {
     this.staticData = await StaticService.get("/my_profile")
@@ -233,9 +250,13 @@ export default {
       const response = await HttpService.post("/user-profile", this.userProfile.profile);
       if (response.status === 200) {
         this.userProfile = response.data;
+        this.editUser = true
       } else {
       // TODO
       }
+    },
+    editUserProfile () {
+      this.editUser = false
     },
     copyText () {
       // TODO
