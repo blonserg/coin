727f82459ecfbf1c909bc59c9b4b76075f2234f7
blonserg <sergforweb@gmail.com>
2021-11-27 11:55:55 +0300

    fix hotfix

--- pages/login.vue
+++ pages/login.vue
@@ -98,7 +98,7 @@ export default {
       const path = "/forgot-password?email=" + this.user.email;
       const response = await HttpService.post(path);
       if (response.status === 200) {
-        console.log(response);
+        // console.log(response);
         // TODO inform user, that email was send
       } else {
       // TODO
