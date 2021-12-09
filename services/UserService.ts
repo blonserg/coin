ccd645b0167324e1df23af9c0508aa8b3872fb8a
Vira <virasmetana@gmail.com>
2021-12-09 16:10:19 +0200

    fix recovery password

--- services/UserService.ts
+++ services/UserService.ts
@@ -34,17 +34,17 @@ export default {
       return response.errors;
     }
   },
-  async recoveryPassword (recoveryKey: string, password: string, passwordConfirmation: string): Promise<string[] | false> {
+  async recoveryPassword (recoveryEmail: string, recoveryKey: string, password: string, passwordConfirmation: string): Promise<string[] | false> {
     const path = "/reset-password";
     const params = {
+      "email": recoveryEmail,
       "token": recoveryKey,
       password,
       "password_confirmation": passwordConfirmation
     };
     const response = await HttpService.post(path, undefined, params);
     if (response.status === 200) {
-      const userData: UserModel = response.data.user;
-      localStorage.setItem("userToken", userData.api_token);
+      localStorage.setItem("userToken", response.data.api_token);
       return false;
     } else {
       return response.errors;
