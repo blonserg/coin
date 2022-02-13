9e8159fcb2d620a3820247c18527b6e56d5b7a16
Sergei Barsukov <sergforweb@gmail.com>
2022-02-13 10:53:43 +0300

    add elemnts at learn item

--- pages/learnitem.vue
+++ pages/learnitem.vue
@@ -7,6 +7,32 @@
         </svg>
         Обучение
       </NuxtLink>
+      <div class="learn-nav mb-7">
+        <div class="d-flex align-center">
+          <div class="learn-nav-info mr-4 d-flex align-center">
+            <LearnPlay class="mr-2" />
+            <span>#10 урок</span>
+          </div>
+          <a class="article-link d-flex align-center mr-2" href="">
+            <ArrowRight class="article-link-prev" />
+          </a>
+          <a class="article-link d-flex align-center mr-2" href="">
+            <ArrowRight class="article-link-prev mr-3 ml-1" />
+            <span>предыдущий урок</span>
+          </a>
+          <a class="article-link d-flex align-center" href="">
+            <span>следующий урок</span>
+            <ArrowRight class="mr-1 ml-3" />
+          </a>
+        </div>
+      </div>
+      <v-row>
+        <v-col md="12">
+          <div class="article-invest_img">
+            <img src="/article.png" alt="">
+          </div>
+        </v-col>
+      </v-row>
       <v-row>
         <v-col md="8">
           <h1 class="article-main_ttl">
@@ -14,28 +40,88 @@
           </h1>
         </v-col>
       </v-row>
-      <v-row>
-        <v-col md="10">
-          <div class="article-invest_img">
-            <img src="/article.png" alt="">
+      <div class="d-flex align-center justify-space-between mb-7">
+        <div class="learn-list-bottom d-flex">
+          <div class="learn-list-label">
+            <LearnPlay class="mr-2" />
+            10 уроков
           </div>
+          <div class="learn-list-label">
+            <LearnTime class="mr-2" />
+            10+ часов
+          </div>
+        </div>
+        <div class="learn-speaker d-flex align-center">
+          <span>Спикеры:</span>
+          <span>Алексей Алексеев</span>
+          <span>Алексей Алексеев</span>
+        </div>
+      </div>
+      <v-row class="mb-2">
+        <v-col md="10">
+          <p>
+            В 2020 году многие авиаперевозчики оказались на грани банкротства после ввода карантинных ограничений в весенне-летний период. От этого пассажиропоток упал более чем на 90%, согласно данным TSA — агентства в США, которое отслеживает, сколько людей в аэропорту входят в зоны безопасности.
+          </p>
+          <p>
+            В 2020 году многие авиаперевозчики оказались на грани банкротства после ввода карантинных ограничений в весенне-летний период. От этого пассажиропоток упал более чем на 90%, согласно данным TSA — агентства в США, которое отслеживает, сколько людей в аэропорту входят в зоны безопасности.
+          </p>
+          <p>
+            В 2020 году многие авиаперевозчики оказались на грани банкротства после ввода карантинных ограничений в весенне-летний период. От этого пассажиропоток упал более чем на 90%, согласно данным TSA — агентства в США, которое отслеживает, сколько людей в аэропорту входят в зоны безопасности.
+          </p>
         </v-col>
       </v-row>
-      <div>
-        <v-row>
-          <v-col md="10">
-            <p>
-              В 2020 году многие авиаперевозчики оказались на грани банкротства после ввода карантинных ограничений в весенне-летний период. От этого пассажиропоток упал более чем на 90%, согласно данным TSA — агентства в США, которое отслеживает, сколько людей в аэропорту входят в зоны безопасности.
-            </p>
-            <p>
-              В 2020 году многие авиаперевозчики оказались на грани банкротства после ввода карантинных ограничений в весенне-летний период. От этого пассажиропоток упал более чем на 90%, согласно данным TSA — агентства в США, которое отслеживает, сколько людей в аэропорту входят в зоны безопасности.
-            </p>
-            <p>
-              В 2020 году многие авиаперевозчики оказались на грани банкротства после ввода карантинных ограничений в весенне-летний период. От этого пассажиропоток упал более чем на 90%, согласно данным TSA — агентства в США, которое отслеживает, сколько людей в аэропорту входят в зоны безопасности.
-            </p>
-          </v-col>
-        </v-row>
+      <div class="d-flex align-center">
+        <v-btn
+          class="btn mr-5"
+          color="#2d7bf6"
+          height="48px"
+          to="/learnlist"
+        >
+          Купить
+        </v-btn>
+        <div class="learn-price">
+          <span class="mr-2">Цена</span>
+          <span class="mr-5">100 балов</span>
+        </div>
+        <v-btn
+          class="btn"
+          color="#2d7bf6"
+          height="48px"
+          to="/learnlist"
+        >
+          Пополнить баланс
+        </v-btn>
       </div>
+      <v-card class="learn-bottom">
+        <div class="learn-bottom-txt mb-5">
+          Пройдите тест чтобы lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+        </div>
+        <v-btn
+          class="btn"
+          color="#2d7bf6"
+          height="48px"
+          to="/learnlist"
+        >
+          <span class="mr-2">Пройти тест</span>
+          <LearnTest class="mt-1" />
+        </v-btn>
+      </v-card>
     </div>
   </div>
 </template>
+
+<script>
+import LearnPlay from "~~/components/svg/LearnPlay";
+import LearnTime from "~~/components/svg/LearnTime";
+import ArrowRight from "~~/components/svg/ArrowRight";
+import LearnTest from "~~/components/svg/LearnTest";
+
+export default {
+  components: {
+    LearnPlay,
+    LearnTime,
+    ArrowRight,
+    LearnTest
+  }
+}
+</script>
